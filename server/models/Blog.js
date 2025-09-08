58b072968fc8b8d5a495e5cedf3d3656633631e1
Yogesh <yogeshthakur938920@gmail.com>
2025-09-08 12:58:38 +0530

    good

--- server/models/Blog.js
+++ server/models/Blog.js
@@ -1,16 +1,36 @@
+// import mongoose from "mongoose";
+
+// const blogSchema = new mongoose.Schema({
+
+// title:{type:String, required: true},
+// subTitle:{type:String},
+// description:{type:String, required: true},
+// category:{type:String, required: true},
+// image:{type:String, required: true},
+// isPublished:{type:Boolean, required: true},
+// // author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
+//  author: { type: String, required: true }  
+// }, {timestamps:true });
+
+// const Blog = mongoose.model('Blog', blogSchema);
+// export  default Blog;
+
 import mongoose from "mongoose";
 
-const blogSchema = new mongoose.Schema({
+const blogSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    subTitle: { type: String },
+    description: { type: String, required: true },
+    category: { type: String, required: true },
+    image: { type: String, required: true },
+    isPublished: { type: Boolean, default: false },
+    author: { type: String, required: true }, // you can later change to ObjectId if you have a User model
+  },
+  { timestamps: true }
+);
 
-title:{type:String, required: true},
-subTitle:{type:String},
-description:{type:String, required: true},
-category:{type:String, required: true},
-image:{type:String, required: true},
-isPublished:{type:Boolean, required: true},
-// author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
- author: { type: String, required: true }  
-}, {timestamps:true });
+// Use PascalCase for the model name
+const Blog = mongoose.model("Blog", blogSchema);
 
-const Blog = mongoose.model('blog', blogSchema);
-export  default Blog;
+export default Blog;
